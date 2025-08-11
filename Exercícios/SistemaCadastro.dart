166bb0b33cfe1375a681c6a10b9423e67a613c68
Lucas Henrique Boaratti Silva <47123916859@>
2025-08-11 09:27:02 -0300

    Tratamento de erro no sistema de cadastro adicionado.

--- Exercícios/SistemaCadastro.dart
+++ Exercícios/SistemaCadastro.dart
@@ -1,22 +1,23 @@
 import 'dart:io';
 
 void main() {
-	print("Digite sua idade: ");
-	int idade = int.parse(stdin.readLineSync()!);
+	try { //Tentativa de executar o código
+		print("Digite sua idade: ");
+		int idade = int.parse(stdin.readLineSync()!);
 
-	bool maiorIdade = ehMaiorIdade(idade);
-	print("\nVocê é maior de idade?: ${maiorIdade ? "Sim :D" : "Não :D"}"); //Operador ternário
+		bool maiorIdade = ehMaiorIdade(idade);
+		print("\nVocê é maior de idade?: ${maiorIdade ? "Sim :D" : "Não :D"}"); //Operador ternário
+	}
+	catch (error) { //"Capturando" um erro do código
+		print("Digite um número válido, por favor.");
+	}
 }
 
 bool ehMaiorIdade(int idade) {
 	if (idade >= 18) {
 		return true;
 	}
-	else if (idade < 18) {
-		return false;
-	}
 	else {
-		print("Digite um número válido, por favor.");
 		return false;
 	}
 }
