c829a030775f318f5910da9e0ece68d5e70723a6
Lucas Henrique Boaratti Silva <47123916859@>
2025-08-14 09:26:35 -0300

    Adicionando alguns comentários mais claros.

--- Tratamento_de_Erros/Null_Vazio.dart
+++ Tratamento_de_Erros/Null_Vazio.dart
@@ -1,8 +1,8 @@
 import 'dart:io';
 
 void main() {
-	List menu = []; //Vazio
-	List menu2; //Nulo
+	List menu = []; //Vazio - Tenho estrutura, mas não tenho dados
+	List menu2; //Nulo - Não tenho estrutura e não tenho dados
 
 	String nome = ""; //Vazio
 	String nome2; //Nulo
